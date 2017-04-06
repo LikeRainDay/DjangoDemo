5130c72871b76478407757ddb5eac98777a660c1
houshuai0816 <houshuai0816@126.com>
2017-04-06 16:03:28 +0800

    Initial commit

---
+++ DjangoDemo/urls.py
@@ -0,0 +1,26 @@
+"""DjangoDemo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+# 将项目文件进行导入
+import DjangoDemo.blog.view as bv
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', bv.hello),
+
+]
